3dc3e788187b6fb858c4ba647e2a0c0f5f9f67a3
GODELTECH\i.stupak <i.stupak@godeltech.com>
2019-10-17 15:42:56 +0300

    SpecFlow added

---
+++ UITest.Tests/ShowAfterText.feature
@@ -0,0 +1,8 @@
+﻿Feature: Showing Text
+	I want to see text in the editor
+	
+@mytag
+Scenario: Show text
+	Given I am on the Initial screen
+	When I press the button
+	Then I should see AFTER text in the editor
---
+++ UITest.Tests/ShowAfterTextSteps.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+using Xamarin.UITest;
+
+namespace UITest.Tests
+{
+    [Binding]
+    public class ShowAfterTextSteps
+    {
+        private IApp app;
+
+        [Given("I am on the Initial screen")]
+        public void InitialScreenIsDisplayed()
+        {
+            app = AppInitializer.StartApp(Platform.Android);
+        }
+        
+        [When("I press the button")]
+        public void WhenIPressAdd()
+        {
+            app.Tap(c => c.Marked("button"));
+        }
+
+        [Then("I should see AFTER text in the editor")]
+        public void ThenTheResultShouldBe()
+        {
+            var result = app.Query(c => c.Marked("editor").Text("AFTER"));
+
+            Assert.IsTrue(result.Any());
+        }
+    }
+}
